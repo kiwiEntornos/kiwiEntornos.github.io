f520cbe2e9c552daa19996f1bbd4f817b2fa504f
mrdarip <darioelas2005@gmail.com>
2024-03-06 13:20:05 +0100

    añadidos a la galería los perros grandes

--- data/dog_info_json.js
+++ data/dog_info_json.js
@@ -1,62 +1,72 @@
-
-sizes = ["Pequeño","Mediano","Grande"];
+sizes = ["Pequeño", "Mediano", "Grande"];
 dogs = [
     {
-        "name": "Lewis",
-        "male":false,
-        "size": 1,
-        "description": "Es una perrita pastora belga abandonada atada con una cadena. Es super cariñosa y le encanta jugar con las personas. Nunca ladra ni es agresiva ni con los humanos ni con otros perritos."
+        name: "Lewis",
+        male: false,
+        size: 1,
+        description:
+            "Es una perrita pastora belga abandonada atada con una cadena. Es super cariñosa y le encanta jugar con las personas. Nunca ladra ni es agresiva ni con los humanos ni con otros perritos.",
     },
     {
-        "name": "Eros",
-        "male":true,
-        "size": 2,
-        "description": "Es un pastor belga con mucha energia. Le encanta jugar con los seres humanos y no es nada agresivo."
+        name: "Eros",
+        male: true,
+        size: 2,
+        description:
+            "Es un pastor belga con mucha energia. Le encanta jugar con los seres humanos y no es nada agresivo.",
     },
     {
-        "name": "Lavinia",
-        "male":false,
-        "size": 1,
-        "description": "Es una galguita muy maja y feliz. Le encanta pedir cariño de los seres humanos y el resto de perros le causan indiferencia."
+        name: "Lavinia",
+        male: false,
+        size: 1,
+        description:
+            "Es una galguita muy maja y feliz. Le encanta pedir cariño de los seres humanos y el resto de perros le causan indiferencia.",
     },
     {
-        "name": "Pikachu",
-        "male":true,
-        "size": 2,
-        "description": "Pikachu es un galgo de gran tamaño que le encanta pedir mimos constantemente. Le gusta mucho correr y jugar con otros perros."
+        name: "Pikachu",
+        male: true,
+        size: 2,
+        description:
+            "Pikachu es un galgo de gran tamaño que le encanta pedir mimos constantemente. Le gusta mucho correr y jugar con otros perros.",
     },
     {
-        "name": "Mela",
-        "male":false,
-        "size": 0,
-        "description": "Mela aparecio abandonada y es super simpatica. Le encanta estar rodeada de humanos y otros perritos. No es agresiva y esta deseando encontrar a una familia."
+        name: "Mela",
+        male: false,
+        size: 0,
+        description:
+            "Mela aparecio abandonada y es super simpatica. Le encanta estar rodeada de humanos y otros perritos. No es agresiva y esta deseando encontrar a una familia.",
     },
     {
-        "name": "Milo",
-        "male":true,
-        "size": 0,
-        "description": "Milo es un pequeño lobito super jugueton y cariñoso. Va ha aprovechar la mas minima oportunidad para robarte unas caricias y es super pacifico."
+        name: "Milo",
+        male: true,
+        size: 0,
+        description:
+            "Milo es un pequeño lobito super jugueton y cariñoso. Va ha aprovechar la mas minima oportunidad para robarte unas caricias y es super pacifico.",
     },
     {
-        "name": "Kala",
-        "male":false,
-        "size": 2,
-        "description": "Kala es la bondad perrificada. Es super tranquila y le encanta pasear. Su hobby favorito es tumbarse con los rayos de sol en la cara. No es nada agresiva con otros perros y es super obediente y respetuosa."
+        name: "Kala",
+        male: false,
+        size: 2,
+        description:
+            "Kala es la bondad perrificada. Es super tranquila y le encanta pasear. Su hobby favorito es tumbarse con los rayos de sol en la cara. No es nada agresiva con otros perros y es super obediente y respetuosa.",
     },
     {
-        "name": "Neska",
-        "male":false,
-        "size": 2,
-        "description": "Esta lobita es de las mas cariñosas que vas a encontrar en estas tierras. Siempre esta dispuesta a recibir mimos y se lleva bien con el resto de lobitos. Cuando dos se ladran suele meterse en medio para separarles, haciendo funciones de perro pastor a la antigua usanza. Fue abandonada con sus cachorros recien nacidos, los cuales ya han encontrado una familia feliz, ahora solo queda ella!"
+        name: "Neska",
+        male: false,
+        size: 2,
+        description:
+            "Esta lobita es de las mas cariñosas que vas a encontrar en estas tierras. Siempre esta dispuesta a recibir mimos y se lleva bien con el resto de lobitos. Cuando dos se ladran suele meterse en medio para separarles, haciendo funciones de perro pastor a la antigua usanza. Fue abandonada con sus cachorros recien nacidos, los cuales ya han encontrado una familia feliz, ahora solo queda ella!",
     },
     {
-        "name": "Desider",
-        "size": 2,
-        "description": "Este lobito es realmente precioso. Creemos que es de un criador de perigri debido a su belleza y su marca en la lengua. Es muy sociable con otros seres humanos. Actualmente Neska y el son novios. Desider sufre de leucemia canina y necesita una familia que le cuide."
+        name: "Desider",
+        male: true,
+        size: 2,
+        description:
+            "Este lobito es realmente precioso. Creemos que es de un criador de perigri debido a su belleza y su marca en la lengua. Es muy sociable con otros seres humanos. Actualmente Neska y el son novios. Desider sufre de leucemia canina y necesita una familia que le cuide.",
     },
     {
-        "name": "Dafne",
-        "size": 1,
-        "description": "Dafne es super cariñosa y no piensa perder la oportunidad de recibir mimos. Es super sociable con otros lobitos y lleva mucho tiempo esperando encontrar la familia que le de una segunda oportunidad. Como muchos otros, este tipo de perros son criados para caza y luego abandonados, pero este caso sorprende por su capicidad para vivir en una casita y ser un gran perrito de compañia!"
-    }
+        name: "Dafne",
+        size: 1,
+        description:
+            "Dafne es super cariñosa y no piensa perder la oportunidad de recibir mimos. Es super sociable con otros lobitos y lleva mucho tiempo esperando encontrar la familia que le de una segunda oportunidad. Como muchos otros, este tipo de perros son criados para caza y luego abandonados, pero este caso sorprende por su capicidad para vivir en una casita y ser un gran perrito de compañia!",
+    },
 ];
